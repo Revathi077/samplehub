214df4aca19cf9392335737c6040db6bac41e7b7
Revathi077 <110281228+Revathi077@users.noreply.github.com>
2022-07-31 20:34:37 +0530

    Add files via upload

---
+++ src/main/java/com/packagename/samplehub/SamplehubApplication.java
@@ -0,0 +1,13 @@
+package com.packagename.samplehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packagename/samplehub/SamplehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packagename.samplehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
